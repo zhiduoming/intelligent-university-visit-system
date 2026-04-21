a77d2972e786a551f60a21b060a7347178d1fcc4
zhiduoming <zhiduoming@qq.com>
2026-04-22 00:17:28 +0800

    feat: add product-style static pages and home forwarding

---
+++ src/main/java/com/github/zhiduoming/controller/PageController.java
@@ -0,0 +1,14 @@
+package com.github.zhiduoming.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PageController {
+
+    @GetMapping("/")
+    public String index() {
+        return "forward:/index.html";
+    }
+}
+
